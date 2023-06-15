ea27c607433b83d18b4ca232881170878cd253e3
Danny Andrews <lunde.andrews@gmail.com>
2023-06-15 13:16:06 -0400

    Add vite/react-swc

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
+import react from "@vitejs/plugin-react-swc";
 
 dotenv.config({ path: "../.env" });
 
 export default {
+  plugins: [react()],
   server: {
     proxy: {
       "/api": `http://localhost:${process.env.PORT}`,
